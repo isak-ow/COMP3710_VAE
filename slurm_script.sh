5d847f09f0d54ca771601ad779666843d18c232e
IsakOrlando <isakow@stud.ntnu.no>
2023-09-12 00:29:05 +1000

    added slurm

---
+++ slurm_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --job-name=CIFAR10
+#SBATCH --cpus-per-task 1
+#SBATCH --mail-type=All
+#SBATCH --mail-user=i.wangensteen@uq.net.au
+#SBATCH -o out.txt
+#SBATCH -e err.txt
+#SBATCH --partition=vgpu
+#SBATCH --gres=gpu:1
+
+conda activate cluster_env
+
+python3 ~/COMP3710_VAE/VAE_script.py
